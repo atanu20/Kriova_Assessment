8068bbbe9fc46db8123cd25a0720e8ea4ffba635
atanu <atanuj383@gmail.com>
2022-10-18 17:33:06 +0530

    Add

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,16 +1,9 @@
 const mysql = require('mysql');
 
-// const dbConnect = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'kriova',
-// });
-
 const dbConnect = mysql.createConnection({
-  host: 'database-1.cinnhgaru4xp.ap-south-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'atanujana',
+  host: 'localhost',
+  user: 'root',
+  password: '',
   database: 'kriova',
 });
 
