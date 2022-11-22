88d61e029e8c3bd2ea977466de9ef9d74b787a73
atanu <atanuj383@gmail.com>
2022-11-22 13:28:35 +0530

    Add

--- src/data/fdata.js
+++ src/data/fdata.js
@@ -1,3 +1,4 @@
 // export const apilink = 'http://localhost:5000';
 
-export const apilink = 'https://emppage.herokuapp.com';
+// export const apilink = 'https://emppage.herokuapp.com';
+export const apilink = 'https://kriova-emp.onrender.com';
